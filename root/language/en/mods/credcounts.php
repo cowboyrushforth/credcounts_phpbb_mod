694575a02055a3ce957c44ae2e0c1d179247993d
scott rushforth <scott@anybeat.com>
2012-06-06 18:04:02 -0700

    initial

---
+++ root/language/en/mods/credcounts.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// umil
+$lang = array_merge($lang, array(
+	'INSTALL_CREDCOUNTS'				=> 'Install the CredCounts Profile Link',
+	'INSTALL_CREDCOUNTS_CONFIRM'		=> 'Are you ready to install the CredCounts Profile Link?',
+
+	'UNINSTALL_CREDCOUNTS'			=> 'Uninstall the CredCounts Profile Link',
+	'UNINSTALL_CREDCOUNTS_CONFIRM'	=> 'Are you ready to uninstall CredCounts Profile Link?',
+	'UPDATE_CREDCOUNTS'				=> 'Update CredCounts Profile Link',
+	'UPDATE_CREDCOUNTS_CONFIRM'		=> 'Are you ready to update CredCounts Profile Link?',
+));
+
+// acp_styles
+$lang = array_merge($lang, array(
+	'IMG_ICON_CONTACT_CREDCOUNTS'				=> 'CredCounts Profile Link',
+));
+
+// everywhere else
+$lang = array_merge($lang, array(
+	'CREDCOUNTS'				=> 'CredCounts',
+	'UCP_CREDCOUNTS'			=> 'CredCounts Email',
+));
